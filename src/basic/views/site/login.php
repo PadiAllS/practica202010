6fbd6fb34ef1d098059979143a67e8ab9a4b26a1
gianranucci <gian_sao@live.com.ar>
2021-08-03 16:59:09 -0300

    nuevo

--- src/basic/views/site/login.php
+++ src/basic/views/site/login.php
@@ -7,21 +7,29 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
+
+
 $this->title = 'Login';
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
+
 <div class="container bg-info bv-example-row text-center text-light">
     <div class="col-8 container-fluid">
         <div class="site-login">
-        <div class="mb-12 p-3 bg-dark">
-            <h1><?= Html::encode($this->title) ?></h1>
-        </div>
-            
+            <div class="mb-12 p-3 bg-dark">
+                <h1><?= Html::encode($this->title) ?></h1>
+            </div>
+
             <p>Complete los siguientes datos para iniciar sesión:</p>
             <div class="row">
                 <?php $form = ActiveForm::begin([
                     'id' => 'login-form',
                     'layout' => 'horizontal',
+                    'fieldConfig' => [
+                        //'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                        'template' => "{label}\n<div class=\"col-sm-12\">{input}</div>\n<div class=\"col-sm-12\">{error}</div>",
+                        'labelOptions' => ['class' => 'col-sm-12 control-label'],
+                    ],
                 ]); ?>
             </div>
 
@@ -42,4 +50,3 @@ $this->title = 'Login';
         </div>
     </div>
 </div>
-
